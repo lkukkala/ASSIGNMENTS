a81e95da0ca1d9d607d33b1923770273b3e36777
lkukkala <149366339+lkukkala@users.noreply.github.com>
2023-11-22 20:47:40 +0530

    Add files via upload

---
+++ 22-11-23-springboot-blog-rest-api/src/main/java/com/cg/entity/Role.java
@@ -0,0 +1,45 @@
+package com.cg.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name="roles1")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private String name;
+	public Role(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", name=" + name + "]";
+	}
+	public Role() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
